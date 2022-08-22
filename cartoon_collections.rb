293bf3c28a392f6466cb879d8601bf31f1b5f57d
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-08-22 23:11:34 +0300

    commit tree 09b57f25fa74eb3c3ad5617af26215c5796fbe58

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each.with_index {|dwarve, i| puts "#{i+1}. #{dwarve}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planteers)# code an argument here
   # Your code here
+  planteers.map {|planteer| planteer.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planteers)# code an argument here
   # Your code here
+  if planteers.any? {|planteer| planteer.size > 4}
+    return true
+  elsif planteers.all? {|planteer| planteer.size <= 4}
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(items)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  find = cheese_types.map do |type|
+    items.find { |item| type == item }
+  end
+
+  return find.empty?? find : find[0]
 end
